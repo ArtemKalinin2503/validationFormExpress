9291d0b285faa57ca703b00eb8cc1fbc0661eb98
Артем Калинин <gennadijkalinin@MacBook-Pro.local>
2019-06-14 13:44:20 +0300

    forma отправляеться

--- src/js/autorization.js
+++ src/js/autorization.js
@@ -4,12 +4,20 @@ $(document).ready(function() {
 		var inputPhone = $('.autorization__input_phone');
 		var inputPassword = $('.autorization__input_password');
     var autorizationButton = $('.autorization__button');
+    var test = $('#dataForm');
     
-    autorizationButton.on('submit', function(){
+    autorizationForm.on('submit', function(e){
       e.preventDefault();
-      if (!inputPhone.val() && inputPassword.val()) {
-        autorizationForm.addClass('autorizationForm_novalid')
-      }
+      console.log("!")
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:3080/test",
+        data: autorizationForm.serialize(), 
+        success: function(data)
+           {
+            test.html(data) 
+           }
+      });
     });
  
 
