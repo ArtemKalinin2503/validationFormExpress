bf676aee2e1d3cc6b80cd6e5cb2cfa42cb011636
kalinin <kalinin@inventive.ru>
2019-06-17 16:13:54 +0300

    work end

--- Express/server.js
+++ Express/server.js
@@ -1,19 +1,19 @@
 //Подключим express
 var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use( cors({ origin: '*' }) );
+app.use(cors({ origin: '*' }));
 
-app.get('/', function (req, res) {
-   res.send('Hello Api') 
+app.get('/', function(req, res) {
+	res.send('Hello Api');
 });
 
-app.listen(3080, function() {
-    console.log('Api app started')
+app.listen(3012, function() {
+	console.log('Api app started');
 });
 
 //Команда для запуска сервера node server.js
@@ -21,15 +21,15 @@ app.listen(3080, function() {
 //Post запрос для формы
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.get("/test", urlencodedParser, function (request, response) {
-  response.sendFile(__dirname + "../index.html");
+app.get('/test', urlencodedParser, function(request, response) {
+	response.sendFile(__dirname + '../index.html');
 });
-app.post("/test", urlencodedParser, function (request, response) {
-  if (!request.body) return response.sendStatus(400);
-  console.log(request.body);
-  response.send(`${'Телефон:'+ request.body.phone} - ${'Пароль:' + request.body.password}`);
+app.post('/test', urlencodedParser, function(request, response) {
+	if (!request.body) return response.sendStatus(400);
+	console.log(request.body);
+	response.send(`${'Телефон:' + request.body.phone} - ${'Пароль:' + request.body.password}`);
 });
 
-app.get("/", function (request, response) {
-  response.send("Главная страница");
+app.get('/', function(request, response) {
+	response.send('Главная страница');
 });
--- build/js/script.js
+++ build/js/script.js
@@ -3,28 +3,108 @@
 
 $(document).ready(function() {
 	function autorization() {
-    var autorizationForm = $('.autorization__form');
+		var autorizationForm = $('.autorization__form');
+		var autorizationModal = $('.autorization__modal');
+		var buttonClose = $('.autorization__button-close');
 		var inputPhone = $('.autorization__input_phone');
 		var inputPassword = $('.autorization__input_password');
     var autorizationButton = $('.autorization__button');
-    var test = $('#dataForm');
-    
-    autorizationForm.on('submit', function(e){
-      e.preventDefault();
-      console.log("!")
-      $.ajax({
-        type: "POST",
-        url: "http://localhost:3080/test",
-        data: autorizationForm.serialize(), 
-        success: function(data)
-           {
-            test.html(data) 
-           }
-      });
-    });
- 
+		var form = $('#dataForm');
+    var autorizationItemPhone = $('.autorization__form-item-phone');
+    var autorizationItemPassword = $('.autorization__form-item-password');
 
-	}
+		autorizationForm.on('submit', function(e) {
+			e.preventDefault();
+			$.ajax({
+				type: 'POST',
+				url: 'http://localhost:3012/test',
+				data: autorizationForm.serialize(),
+				success: function(data) {
+					form.html(data);
+				}
+			});
+			autorizationModal.addClass('autorization__modal_active');
+		});
+
+		//Кнопка закрыть модальное окно
+		buttonClose.on('click', function() {
+			autorizationModal.removeClass('autorization__modal_active');
+		});
+
+    //Валидация поля пароль
+    function validationPassword(value) {
+      if(value.val().length < 5) {
+        autorizationItemPassword.addClass('autorization__input_novalid');
+        autorizationItemPassword.removeClass('autorization__input_valid');
+      } else {
+        autorizationItemPassword.addClass('autorization__input_valid');
+        autorizationItemPassword.removeClass('autorization__input_novalid');
+      }
+    };
+
+    //Валидация кнопки Войти
+    function validationBtn(value) {
+      //Блокировка кнопки Войти если поля не валидны
+      if (value.hasClass('autorization__input_novalid')) {
+        autorizationButton.attr('disabled', 'disabled');
+        autorizationButton.addClass('autorization__button_disabled');
+      } else {
+        autorizationButton.removeAttr("disabled");
+        autorizationButton.removeClass('autorization__button_disabled');
+      }
+      if (inputPassword.val().length < 5 || autorizationItemPhone.hasClass('autorization__input_novalid')) {
+        autorizationButton.attr('disabled', 'disabled');
+        autorizationButton.addClass('autorization__button_disabled')
+      } else {
+        autorizationButton.removeAttr("disabled");
+        autorizationButton.removeClass('autorization__button_disabled')
+      }
+    };
+
+    //Проверка на длину пароля (не менее 5 символов)
+    inputPassword.on('input', function() {
+      validationPassword($(this));
+      validationBtn($(this))
+    });
+
+    //Валидация поля телефон
+    inputPhone.on('input', function() {
+        validationBtn(autorizationItemPhone);
+        var inputValue = getInputValue();
+        var length = inputValue.length; 
+        if (inputValue < 1000)
+        {
+            inputValue = '('+inputValue;
+        }else if (inputValue < 1000000) 
+        {
+            inputValue = '('+ inputValue.substring(0, 3) + ')' + inputValue.substring(3, length);
+        }else if (inputValue < 10000000000) 
+        {
+            inputValue = '('+ inputValue.substring(0, 3) + ')' + inputValue.substring(3, 6) + '-' + inputValue.substring(6, length);
+        }else
+        {
+            inputValue = '('+ inputValue.substring(0, 3) + ')' + inputValue.substring(3, 6) + '-' + inputValue.substring(6, 10);
+        }       
+        inputPhone.val(inputValue);
+        inputValue = getInputValue();
+      if ((inputValue > 2000000000) && (inputValue < 9999999999))
+      {
+          autorizationItemPhone.addClass('autorization__input_valid'); autorizationItemPhone.removeClass('autorization__input_novalid');
+      }else
+      {
+         autorizationItemPhone.addClass('autorization__input_novalid'); autorizationItemPhone.removeClass('autorization__input_valid');
+      }
+  });
+
+  function getInputValue() {
+        var inputValue = inputPhone.val().replace(/\D/g,'');
+      if (inputValue.charAt(0) == 1)
+      {
+          var inputValue = inputValue.substring(1, inputValue.length);
+      }
+      return inputValue;
+  }
+  }
 	autorization();
 });
 
